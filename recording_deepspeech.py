3ab5de1b1831e4410edbdd8af6fb37afd5abafe8
friday <yiyinghahalin@gmail.com>
2017-12-27 10:06:23 +0800

    Update recording_deepspeech.py

--- recording_deepspeech.py
+++ recording_deepspeech.py
@@ -2,13 +2,15 @@ import os
 import datetime
 
 recorded = []
-for dn, _, fs in os.walk("new_data/"):
+for dn, _, fs in os.walk("data/self/"):
     name = dn.split("/")[1]
     if name:
         recorded.append(name)
 
-with open("new_data/recording_scripts.txt", "r") as f:
+with open("data/self/recording_scripts.txt", "r") as f:
     scripts = f.readlines()
+
+# record one command by 20 times
 scripts_20 = []
 for idx, src in enumerate(scripts):
     src_20 = [(idx, src.replace("\n", ""))] * 20
@@ -23,8 +25,8 @@ while True:
     else:
         break
 
-os.mkdir("new_data/" + person)
-person_folder = "new_data/" + person + "/"
+os.mkdir("data/self/" + person)
+person_folder = "data/self/" + person + "/"
 
 for count, value in enumerate(scripts_20):
     idx = value[0]
@@ -32,6 +34,5 @@ for count, value in enumerate(scripts_20):
     filename = str(datetime.date.today()).replace("-", '') + "_" + person + "_" + str(count+1) + "_" + str(idx) + ".wav"
     print("-" * 60 + "\n{}: {}\n".format(count+1, script))
     input("Press ENTER and start recording >>> ")
-    os.system('arecord -D plughw:1,0 -d 5 -r 16000 -f S16_LE ' + person_folder + filename)
-#    os.system("arecord -d 5 -r 16000 -f S16_LE " + person_folder + filename)
+    os.system("arecord -d 5 -r 16000 -f S16_LE " + person_folder + filename)
 
